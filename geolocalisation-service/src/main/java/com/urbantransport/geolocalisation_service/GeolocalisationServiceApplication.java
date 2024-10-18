841d3c526c2f36dcbaba8998621e0af22e1e894d
fadliSaber <saberfadli99@gmail.com>
2024-10-18 22:09:17 +0100

    Initialization of geolocalisation, notification & subscription services

---
+++ geolocalisation-service/src/main/java/com/urbantransport/geolocalisation_service/GeolocalisationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.urbantransport.geolocalisation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeolocalisationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeolocalisationServiceApplication.class, args);
+	}
+
+}
