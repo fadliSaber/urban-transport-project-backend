133c8cbd04103531c06364254cbfd26632c3141e
fadliSaber <saberfadli99@gmail.com>
2024-10-18 21:59:43 +0100

    Initialization of route-schedule-service

---
+++ route-schedule-service/src/main/java/com/urbantransport/route_schedule_service/RouteScheduleServiceApplication.java
@@ -0,0 +1,13 @@
+package com.urbantransport.route_schedule_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteScheduleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteScheduleServiceApplication.class, args);
+	}
+
+}
---
+++ route-schedule-service/src/test/java/com/urbantransport/route_schedule_service/RouteScheduleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbantransport.route_schedule_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteScheduleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
