0538c7c3e4d1a9fec168f477ca3686a76e5976cf
Ilyass Krichi <mc.mono20@gmail.com>
2024-11-17 17:52:59 +0100

    chore: update the controller

--- ticket-service/src/main/java/com/urbantransport/ticket_service/controller/TicketController.java
+++ ticket-service/src/main/java/com/urbantransport/ticket_service/controller/TicketController.java
@@ -8,6 +8,10 @@ import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,13 +23,13 @@ public class TicketController {
 
   private final TicketService ticketService;
 
-  @RequestMapping("/purchase")
+  @PostMapping("/purchase")
   public ResponseEntity<Ticket> purchaseTicket(@RequestBody Ticket ticket)
     throws StripeException {
     return ResponseEntity.ok(ticketService.purchaseTicket(ticket));
   }
 
-  @RequestMapping("/finalize")
+  @PostMapping("/finalize")
   public ResponseEntity<String> finalizePurchase(
     @RequestBody FinalizePurchaseRequest finalizePurchaseRequest
   ) {
@@ -35,17 +39,17 @@ public class TicketController {
     return ResponseEntity.ok("Purchase finalized");
   }
 
-  @RequestMapping("/getticket")
+  @GetMapping("/getticket")
   public ResponseEntity<Ticket> getTicket(@RequestBody UUID id) {
     return ResponseEntity.ok(ticketService.getTicket(id));
   }
 
-  @RequestMapping("/alltickets")
+  @GetMapping("/alltickets")
   public ResponseEntity<List<Ticket>> getAllTickets() {
     return ResponseEntity.ok(ticketService.getAllTickets());
   }
 
-  @RequestMapping("/updateticket")
+  @PutMapping("/updateticket")
   public ResponseEntity<Ticket> updateTicket(
     @RequestBody UUID id,
     @RequestBody Ticket newticket
@@ -53,7 +57,7 @@ public class TicketController {
     return ResponseEntity.ok(ticketService.updateTicket(id, newticket));
   }
 
-  @RequestMapping("/deleteticket")
+  @DeleteMapping("/deleteticket")
   public ResponseEntity<String> deleteTicket(@RequestBody UUID id) {
     ticketService.deleteTicket(id);
     return ResponseEntity.ok("Ticket deleted");
